48a5a2d3db12462ac3408dff210e083b1dd7f525
RizalSR <rizalsaidramadhan98@gmail.com>
2019-11-24 05:06:01 +0700

    Script Jalan

---
+++ Ghost Trap Adventure/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public ControllerMovement controller;
+    public float runSpeed = 40f;
+    private float horizontalMove = 0f;
+    private bool jump = false;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump );
+        jump = false;
+    }
+}
