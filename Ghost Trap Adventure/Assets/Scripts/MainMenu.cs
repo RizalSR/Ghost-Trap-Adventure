d7b8a874055038e770ea032c7f0053b2c1f6f3f3
Setiadi Pratama <idaites23@gmail.com>
2019-11-23 13:03:14 +0700

    MenuDone

---
+++ Ghost Trap Adventure/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void Controls()
+    {
+        SceneManager.LoadScene("Controls");
+    }
+
+    public void KeluarPermainan()
+    {
+        Application.Quit();
+        Debug.Log("Keluar");
+    }
+
+    public void Back()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
