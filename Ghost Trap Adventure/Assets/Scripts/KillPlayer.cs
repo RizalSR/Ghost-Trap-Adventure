6af020196e3197fba93f014f2c26176d78b835ac
RizalSR <rizalsaidramadhan98@gmail.com>
2019-11-25 23:31:37 +0700

    Kill and Spawn

---
+++ Ghost Trap Adventure/Assets/Scripts/KillPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillPlayer : MonoBehaviour
+{
+    [SerializeField] public Transform spawnPoint;
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.transform.CompareTag("Player"))
+        {
+            other.transform.position = spawnPoint.position;
+        }
+    }
+}
