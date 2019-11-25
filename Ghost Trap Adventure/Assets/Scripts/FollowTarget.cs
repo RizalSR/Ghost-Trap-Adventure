57fd71217d034f323ce1935a553440a9d6a2dc1f
Rohalyasals <rohalya.salsabila@gmail.com>
2019-11-25 19:17:48 +0700

    coin destroy

--- Ghost Trap Adventure/Assets/Scripts/FollowTarget.cs
+++ Ghost Trap Adventure/Assets/Scripts/FollowTarget.cs
@@ -3,9 +3,9 @@ using UnityEngine;
 public class FollowTarget : MonoBehaviour
 {
     public Transform player;
-    public Transform Bg1;
-    public Transform Bg2;
-    public Transform Bg3;
+    // public Transform Bg1;
+    // public Transform Bg2;
+    // public Transform Bg3;
 
     // Use this for initialization
     void Start()
@@ -21,9 +21,9 @@ public class FollowTarget : MonoBehaviour
             transform.position = Vector3.Lerp(transform.position, new Vector3(player.position.x, transform.position.y, transform.position.z), 0.1f);
         }
         
-        Bg1.transform.position = new Vector2(transform.position.x * 1.0f, Bg1.transform.position.y);
-        Bg2.transform.position = new Vector2(transform.position.x * 0.8f, Bg2.transform.position.y);
-        Bg3.transform.position = new Vector2(transform.position.x * 0.6f, Bg3.transform.position.y);
+        // Bg1.transform.position = new Vector2(transform.position.x * 1.0f, Bg1.transform.position.y);
+        // Bg2.transform.position = new Vector2(transform.position.x * 0.8f, Bg2.transform.position.y);
+        // Bg3.transform.position = new Vector2(transform.position.x * 0.6f, Bg3.transform.position.y);
     
     }
 }
