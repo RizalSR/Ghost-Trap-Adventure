a958e4db6581b37955ad54405e1854c5019e2c19
RizalSR <rizalsaidramadhan98@gmail.com>
2019-11-26 02:35:31 +0700

    Level1

---
+++ Ghost Trap Adventure/Assets/Scripts/FallingObject.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingObject : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    public float speed = 300.0f;
+    [SerializeField]
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb.GetComponents<Rigidbody2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.name.Equals("Player"))
+        {
+            rb.isKinematic = false;
+            rb.AddForce(Vector2.down*speed);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.name.Equals("Player"))
+        {
+            Debug.Log("Got You!!");
+        }
+    }
+}
